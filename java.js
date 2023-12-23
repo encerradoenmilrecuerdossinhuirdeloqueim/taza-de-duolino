39096c530169eb4e0938082c55ae651982123d2b
encerradoenmilrecuerdossinhuirdeloqueim <153873426+encerradoenmilrecuerdossinhuirdeloqueim@users.noreply.github.com>
2023-12-23 17:58:25 -0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,48 @@
+
+var detector= "";
+var status= "";
+var objects= [];
+function preload()
+{
+    img=loadImage("buho-verde-en-oficina.jpg");
+}
+
+function setup()
+{
+
+    canvas=createCanvas(1500, 700);
+    canvas.center();
+    objectDetector=ml5.objectDetector("cocossd", nombre);
+    document.getElementById("status").innerHTML="archivo";
+} 
+function nombre()
+{
+  console.log("arsivo");
+  status=true;
+}
+function ultfunción(error, results)
+{
+if (error){
+  console.error(error);
+}   
+else {
+console.log(results);
+objects=results;
+}
+}
+function draw()
+{
+image(img, 0, 0, 1500, 700);
+if (status!="") 
+  { 
+  objectDetector.detect(img, ultfunción);
+  for (var ol=0; ol<objects.length; ol++)
+  {
+    fill("red");
+    var porcent=floor(objects[ol].confidence * 100);
+    text (objects[ol].label+" "+porcent+"%",objects[ol].x+10,objects[ol].y+15);
+    noFill(); stroke(255, 0, 0); rect(objects[ol].x, objects[ol].y, objects[ol].width, objects[ol].height);
+  }
+}
+}
+
---
+++ main.js
@@ -0,0 +1,12 @@
+function funcion()
+ {
+    window.location="indx.html"
+ }
+ function funtion()
+ {
+    window.location="indek.html"
+ }
+ function funktion()
+ {
+    window.location="inde.html"
+ }
